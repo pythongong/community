41f80766bf532201e2eec13c4a7bf373461fdffd
pythongong <gongcheng2018@foxmail.com>
2025-04-30 20:39:14 +0800

    init

--- user/src/main/java/com/pythongong/community/UserApplication.java
+++ user/src/main/java/com/pythongong/community/UserApplication.java
@@ -1,4 +1,4 @@
-package com.pythongong.community.user;
+package com.pythongong.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ user/src/main/java/com/pythongong/community/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.pythongong.community.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import reactor.core.publisher.Mono;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/hello")
+    public Mono<String> handle() {
+        return Mono.just("Hello WebFlux");
+    }
+
+}
